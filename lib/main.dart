f74b0f1c064dbafcfc1485f08cbcd83506410268
Symphatico <Symphatico>
2022-04-26 02:33:43 -0700

    r

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:about_me/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const AboutMeApp());
@@ -13,11 +14,3 @@ class AboutMeApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget{
-  const HomePage({Key?key}):super(key: key);
-  
-  @override  
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
